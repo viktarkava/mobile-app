1d16e0bd70310f1c27dc5ab68ac2bb20c7798f6c
Viktar Kavalenka <vkavalenka@vestmark.com>
2022-11-02 04:51:44 -0400

    More exception handling

---
+++ src/main/java/com/example/demo/ui/model/response/ErrorMessge.java
@@ -0,0 +1,33 @@
+package com.example.demo.ui.model.response;
+
+import java.util.Date;
+
+public class ErrorMessge {
+	private Date timestamp;
+	private String message;
+
+	public ErrorMessge() {
+	}
+
+	public ErrorMessge(Date timestamp, String message) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
